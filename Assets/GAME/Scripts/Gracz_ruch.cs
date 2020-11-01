8c373f67505bc2280997f895e025b557ea68b2fa
Antoni Gawlikowski <tefloon@gmail.com>
2020-11-01 14:15:32 +0100

    Odcinek 2. Ruch statku. Ograniczenie ruchu.

---
+++ Assets/GAME/Scripts/Gracz_ruch.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gracz_ruch : MonoBehaviour
+{
+    [SerializeField] float predkoscRuchu;
+    [SerializeField] float ograniczenieGoraDol;
+    [SerializeField] float ograniczeniePrawaLewa;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ruch();
+    }
+
+    void Ruch()
+    {
+        Vector3 kierunek = Vector3.zero;
+
+        if ((Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) 
+            && transform.position.x > -ograniczeniePrawaLewa )
+        {
+            kierunek += Vector3.left;
+        }
+
+        if ((Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+            && transform.position.x < ograniczeniePrawaLewa)
+        {
+            kierunek += Vector3.right;
+        }
+
+        if ((Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+            && transform.position.z < ograniczenieGoraDol)
+        {
+            kierunek += Vector3.forward;
+        }
+
+        if ((Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+             && transform.position.z > -ograniczenieGoraDol)
+        {
+            kierunek += Vector3.back;
+        }
+
+        Vector3 przesuniecie = kierunek * predkoscRuchu * Time.deltaTime;
+
+        transform.Translate(przesuniecie);
+    }
+}
